221824766d3271be9729d92dc292f922e04cd54a
srbwin1 <srbwin@gmail.com>
2015-10-06 21:25:48 -0400

    Added the Java Project for testing

---
+++ src/com/srb/Hello.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.srb;
+
+/**
+ * @author srbwin
+ *
+ */
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(printHelloWorld());
+	}
+	
+	private static String printHelloWorld() {
+		return "Thanks Colaberry!";
+		
+	}
+}
