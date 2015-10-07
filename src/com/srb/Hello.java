7c2d6d27b7ddf0c948d0745c60bc43a5c73fbdfd
srbwin1 <srbwin@gmail.com>
2015-10-06 23:03:40 -0400

    Added the code files

--- src/com/srb/Hello.java
+++ src/com/srb/Hello.java
@@ -10,15 +10,29 @@ package com.srb;
 
 public class Hello {
 
+	int flighttype;
+	
+	public int getFlighttype() {
+		return flighttype;
+	}
+
+	public void setFlighttype(int flighttype) {
+		this.flighttype = flighttype;
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println(printHelloWorld());
+		Hello a = new Hello();
+		a.setFlighttype(1234);
+		System.out.println(a.getFlighttype());
 	}
 	
 	private static String printHelloWorld() {
 		return "Thanks Colaberry!";
+		
 		
 	}
 }
